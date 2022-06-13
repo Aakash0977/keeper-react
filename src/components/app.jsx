b34bae99f43a874f655d61f59b468acec4d41821
Aakash <aakash0977@gmail.com>
2022-06-13 19:58:41 +0545

    updated with arrow function

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Footer from "./footer";
 import Header from "./header";
 import Note from "./note";
+import notes from "../notes"
 
 function App() {
   return (
     <div>
       <Header />
       <Footer />
-      <Note />
+      {notes.map(newNotes => <Note  title = {newNotes.title} 
+        content ={newNotes.content}
+      />)};
     </div>
   );
 }
